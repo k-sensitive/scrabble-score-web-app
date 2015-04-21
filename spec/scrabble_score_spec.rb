6788c1669873be380c573266408e448c5e347c6e
Cory Spitzer & Kim Dang <student@epicodus.com>
2015-04-21 11:55:38 -0700

    Add 'q' and whole word test.

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -2,7 +2,20 @@ require('rspec')
 require('scrabble_score')
 
 describe('String#scrabble_score') do
+
+  # Letter 'a' gives a score of 1.
   it("returns a scrabble score for a letter") do
     expect("a".scrabble_score()).to(eq(1))
   end
+
+  # Letter 'q' gives a score of 10.
+  it("returns a scrabble score for the letter q") do
+    expect("q".scrabble_score()).to(eq(10))
+  end
+
+  # Calculates the score of a full word
+  it("Calculates the score of a full word") do
+    expect("Hash".scrabble_score()).to(eq(10))
+  end
+
 end
